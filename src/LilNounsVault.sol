080a2637627ef2fd893d376d712a23d6afbc323a
Milad Nekofar <milad@nekofar.com>
2024-08-10 16:18:59 +0400

    feat(vault): add pausable functionality
    Include pausable functionality to allow for emergency stops.

--- src/LilNounsVault.sol
+++ src/LilNounsVault.sol
@@ -1,21 +1,28 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.26;
 
-// Importing OpenZeppelin's Initializable and UUPSUpgradeable contracts
+// Importing OpenZeppelin's modules
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 
 /**
  * @title LilNounsVault
  * @dev This contract serves as a vault, upgradeable via UUPS pattern.
- * It is designed to be a base template, with additional logic to be added as needed.
+ * It includes pausable functionality to allow for emergency stops.
  */
-contract LilNounsVault is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract LilNounsVault is
+  Initializable,
+  UUPSUpgradeable,
+  OwnableUpgradeable,
+  PausableUpgradeable
+{
   /// @notice Initializer function to replace the constructor for upgradeable contracts
   function initialize() public initializer {
     __Ownable_init();
     __UUPSUpgradeable_init();
+    __Pausable_init();
   }
 
   /**
@@ -26,4 +33,20 @@ contract LilNounsVault is Initializable, UUPSUpgradeable, OwnableUpgradeable {
   function _authorizeUpgrade(
     address newImplementation
   ) internal override onlyOwner {}
+
+  /**
+   * @notice Pauses the contract, preventing all state-changing operations
+   * @dev Only the owner can pause the contract
+   */
+  function pause() external onlyOwner {
+    _pause();
+  }
+
+  /**
+   * @notice Unpauses the contract, resuming all state-changing operations
+   * @dev Only the owner can unpause the contract
+   */
+  function unpause() external onlyOwner {
+    _unpause();
+  }
 }
