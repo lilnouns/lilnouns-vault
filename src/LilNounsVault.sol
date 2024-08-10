b87c27721620ff04cb9ba1eb0b760288b2d7930e
Milad Nekofar <milad@nekofar.com>
2024-08-11 01:08:28 +0400

    feat(vault): add timed pause functionality
    introduce `pauseStart` and `pauseEnd` to enable timed contract pauses, with added checks for invalid pause periods and restricted unpauses.

--- src/LilNounsVault.sol
+++ src/LilNounsVault.sol
@@ -21,13 +21,52 @@ contract LilNounsVault is
   IERC721Receiver
 {
   /// @notice Custom error for paused contract during upgrade attempt
-  error ContractPaused();
+  error ContractPausedDuringUpgrade();
+
+  /// @notice Error for when attempting to unpause during the restricted period
+  error UnpauseRestricted();
+
+  /// @notice Error for invalid pause period
+  error InvalidPausePeriod();
+
+  uint256 public pauseStart;
+  uint256 public pauseEnd;
 
   /// @notice Initializer function to replace the constructor for upgradeable contracts
   function initialize() public initializer {
     __Ownable_init(msg.sender);
     __UUPSUpgradeable_init();
     __Pausable_init();
+  }
+
+  /**
+   * @notice Pauses the contract for a specific period.
+   * @dev Only the owner can pause the contract. The pause start and end times are set as timestamps.
+   * @param _pauseStart The timestamp when the pause period begins.
+   * @param _pauseEnd The timestamp when the pause period ends.
+   */
+  function pause(uint256 _pauseStart, uint256 _pauseEnd) external onlyOwner {
+    if (_pauseEnd <= _pauseStart) {
+      revert InvalidPausePeriod();
+    }
+    if (_pauseStart < block.timestamp) {
+      revert InvalidPausePeriod();
+    }
+
+    pauseStart = _pauseStart;
+    pauseEnd = _pauseEnd;
+    _pause();
+  }
+
+  /**
+   * @notice Unpauses the contract, resuming all state-changing operations.
+   * @dev Only the owner can unpause the contract. Cannot unpause during the restricted period.
+   */
+  function unpause() external onlyOwner {
+    if (block.timestamp >= pauseStart && block.timestamp <= pauseEnd) {
+      revert UnpauseRestricted();
+    }
+    _unpause();
   }
 
   /**
@@ -38,28 +77,14 @@ contract LilNounsVault is
   function _authorizeUpgrade(
     address newImplementation
   ) internal view override onlyOwner {
-    if (paused()) {
-      revert ContractPaused();
+    if (
+      paused() && (block.timestamp >= pauseStart && block.timestamp <= pauseEnd)
+    ) {
+      revert ContractPausedDuringUpgrade();
     }
 
     // Suppress unused variable warning
     (newImplementation);
-  }
-
-  /**
-   * @notice Pauses the contract, preventing all state-changing operations
-   * @dev Only the owner can pause the contract
-   */
-  function pause() external onlyOwner {
-    _pause();
-  }
-
-  /**
-   * @notice Unpauses the contract, resuming all state-changing operations
-   * @dev Only the owner can unpause the contract
-   */
-  function unpause() external onlyOwner {
-    _unpause();
   }
 
   /**
