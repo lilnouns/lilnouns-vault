02dc8c5604a638a4d83aab0ea39378e82e764823
Milad Nekofar <milad@nekofar.com>
2024-08-10 15:27:26 +0400

    chore(hardhat): register upgrades the plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-foundry";
 import "@nomiclabs/hardhat-solhint";
+import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.26",
