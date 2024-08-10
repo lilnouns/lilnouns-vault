f158838482a08be8122dd6f428d43d4e6447f1f4
Milad Nekofar <milad@nekofar.com>
2024-08-10 17:16:41 +0400

    fix: import to `PausableUpgradeable` from `utils`
    Update the import path for `PausableUpgradeable` to reflect its correct location.

--- src/LilNounsVault.sol
+++ src/LilNounsVault.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.26;
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 
 /**
  * @title LilNounsVault
