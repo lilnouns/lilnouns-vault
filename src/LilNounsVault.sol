c72f6eb8a47fc2372b1f0f5c418e9335f9ed9c56
Milad Nekofar <milad@nekofar.com>
2024-08-11 04:44:24 +0400

    feat(vault): add `withdraw` function for ether transfer
    enable the owner to withdraw ether from the contract.

--- src/LilNounsVault.sol
+++ src/LilNounsVault.sol
@@ -10,6 +10,7 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC721Receiver } from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 
 /**
  * @title LilNounsVault
@@ -94,6 +95,11 @@ contract LilNounsVault is
 
     // Suppress unused variable warning
     (newImplementation);
+  }
+
+  function withdraw() external onlyOwner whenNotPaused {
+    uint256 amount = address(this).balance;
+    Address.sendValue(payable(msg.sender), amount);
   }
 
   /**
