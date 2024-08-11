230c19ad853a1efb3d153050d89d03cfdce9f3d7
Milad Nekofar <milad@nekofar.com>
2024-08-11 18:37:16 +0400

    feat(mocks): add voting extensions to ERC20 and ERC721
    Integrate `ERC20Votes` and `ERC721Votes` for voting functionality.

--- src/mocks/ERC20Mock.sol
+++ src/mocks/ERC20Mock.sol
@@ -2,14 +2,30 @@
 pragma solidity ^0.8.26;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20Permit, Nonces } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import { ERC20Votes } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract ERC20Mock is ERC20 {
+contract ERC20Mock is ERC20, ERC20Permit, ERC20Votes {
   constructor(
     string memory name,
-    string memory symbol,
-    address initialAccount,
-    uint256 initialBalance
-  ) ERC20(name, symbol) {
-    _mint(initialAccount, initialBalance);
+    string memory symbol
+  ) ERC20(name, symbol) ERC20Permit(name) {}
+
+  function mint(address to, uint256 amount) external {
+    _mint(to, amount);
+  }
+
+  function _update(
+    address from,
+    address to,
+    uint256 value
+  ) internal override(ERC20, ERC20Votes) {
+    super._update(from, to, value);
+  }
+
+  function nonces(
+    address owner
+  ) public view override(ERC20Permit, Nonces) returns (uint256) {
+    return super.nonces(owner);
   }
 }
